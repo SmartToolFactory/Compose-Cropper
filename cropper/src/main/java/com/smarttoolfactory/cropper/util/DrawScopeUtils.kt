47a183108c787804a5820074582951e453c59bcb
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-08-25 16:02:30 +0300

    add DrawScope util functions

---
+++ cropper/src/main/java/com/smarttoolfactory/cropper/util/DrawScopeUtils.kt
@@ -0,0 +1,81 @@
+package com.smarttoolfactory.cropper.util
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
+import kotlin.math.cos
+import kotlin.math.sin
+
+
+fun DrawScope.drawGrid(rect: Rect, color: Color = Color.White) {
+
+    val width = rect.width
+    val height = rect.height
+    val gridWidth = width / 3
+    val gridHeight = height / 3
+
+
+    drawRect(
+        color = color,
+        topLeft = rect.topLeft,
+        size = rect.size,
+        style = Stroke(width = 2.dp.toPx())
+    )
+
+    // Horizontal lines
+    for (i in 1..2) {
+        drawLine(
+            color = color,
+            start = Offset(rect.left, rect.top + i * gridHeight),
+            end = Offset(rect.right, rect.top + i * gridHeight),
+            strokeWidth = .7.dp.toPx()
+        )
+    }
+
+    // Vertical lines
+    for (i in 1..2) {
+        drawLine(
+            color,
+            start = Offset(rect.left + i * gridWidth, rect.top),
+            end = Offset(rect.left + i * gridWidth, rect.bottom),
+            strokeWidth = .7.dp.toPx()
+        )
+    }
+}
+
+
+/**
+ * Creates a polygon with number of [sides] centered at ([cx],[cy]) with [radius].
+ * ```
+ *  To generate regular polygons (i.e. where each interior angle is the same),
+ *  polar coordinates are extremely useful. You can calculate the angle necessary
+ *  to produce the desired number of sides (as the interior angles total 360º)
+ *  and then use multiples of this angle with the same radius to describe each point.
+ * val x = radius * Math.cos(angle);
+ * val y = radius * Math.sin(angle);
+ *
+ * For instance to draw triangle loop thrice with angle
+ * 0, 120, 240 degrees in radians and draw lines from each coordinate.
+ * ```
+ */
+fun createPolygonPath(cx: Float, cy: Float, sides: Int, radius: Float): Path {
+    val angle = 2.0 * Math.PI / sides
+
+    return Path().apply {
+        moveTo(
+            cx + (radius * cos(0.0)).toFloat(),
+            cy + (radius * sin(0.0)).toFloat()
+        )
+        for (i in 1 until sides) {
+            lineTo(
+                cx + (radius * cos(angle * i)).toFloat(),
+                cy + (radius * sin(angle * i)).toFloat()
+            )
+        }
+        close()
+    }
+}
