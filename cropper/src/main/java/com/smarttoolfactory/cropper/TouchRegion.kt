f34db07e08136a2bb5e38514aff754cdc314b284
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-08-27 11:12:17 +0300

    add touch region enum for dynamic overlay handle selection

---
+++ cropper/src/main/java/com/smarttoolfactory/cropper/TouchRegion.kt
@@ -0,0 +1,8 @@
+package com.smarttoolfactory.cropper
+
+/**
+ * Enum for detecting which section of Composable user has initially touched
+ */
+internal enum class TouchRegion {
+    TopLeft, TopRight, BottomLeft, BottomRight, Inside, None
+}
