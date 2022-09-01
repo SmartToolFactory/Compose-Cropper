c4f882ad4de93ded875c8b728ed1ce18060ab644
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-09-01 11:06:33 +0300

    add aspect ratio to CropDefaults properties

--- cropper/src/main/java/com/smarttoolfactory/cropper/CropDefaults.kt
+++ cropper/src/main/java/com/smarttoolfactory/cropper/CropDefaults.kt
@@ -2,6 +2,8 @@ package com.smarttoolfactory.cropper
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 
 /**
@@ -13,6 +15,7 @@ object CropDefaults {
         initialZoom: Float = 1f,
         minZoom: Float = 1f,
         maxZoom: Float = 5f,
+        aspectRatio: Float = 1f,
         fling: Boolean = false,
         moveToBounds: Boolean = true,
         zoomable: Boolean = true,
@@ -24,6 +27,7 @@ object CropDefaults {
             initialZoom = initialZoom,
             minZoom = minZoom,
             maxZoom = maxZoom,
+            aspectRatio = aspectRatio,
             fling = fling,
             moveToBounds = moveToBounds,
             zoomable = zoomable,
@@ -36,28 +40,22 @@ object CropDefaults {
     fun style(
         cropType: CropType = CropType.Dynamic,
         drawOverlay: Boolean = true,
-        drawInnerOverlay: Boolean = true,
+        drawGrid: Boolean = true,
+        handleSize: Dp = 40.dp,
+        minCropSize: Dp = 100.dp,
         overlayColor: Color = Color.White
     ): CropStyle {
         return CropStyle(
             cropType = cropType,
             drawOverlay = drawOverlay,
-            drawInnerOverlay = drawInnerOverlay,
+            drawGrid = drawGrid,
+            handleSize = handleSize,
+            minCropSize = minCropSize,
             overlayColor = overlayColor
         )
     }
 }
 
-/**
- * Data class for cropper styling
- */
-@Immutable
-data class CropStyle internal constructor(
-    val cropType: CropType = CropType.Dynamic,
-    val drawOverlay: Boolean = true,
-    val drawInnerOverlay: Boolean = true,
-    val overlayColor: Color = Color.White
-)
 
 /**
  * Data class for selecting cropper properties
@@ -67,10 +65,24 @@ data class CropProperties internal constructor(
     val initialZoom: Float = 1f,
     val minZoom: Float = 1f,
     val maxZoom: Float = 5f,
+    val aspectRatio: Float = 1f,
     val fling: Boolean = false,
     val moveToBounds: Boolean = true,
     val zoomable: Boolean = true,
     val pannable: Boolean = true,
     val rotatable: Boolean = false,
     val limitPan: Boolean = false
+)
+
+/**
+ * Data class for cropper styling
+ */
+@Immutable
+data class CropStyle internal constructor(
+    val cropType: CropType = CropType.Dynamic,
+    val drawOverlay: Boolean = true,
+    val drawGrid: Boolean = true,
+    val handleSize: Dp = 40.dp,
+    val minCropSize: Dp = 100.dp,
+    val overlayColor: Color = Color.White
 )
