557624c6aefb2c23a845b27a23c798d30908ddea
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-09-03 16:04:00 +0300

    change visibility of lerp, scale functions to public

--- cropper/src/main/java/com/smarttoolfactory/cropper/util/DimensionUtil.kt
+++ cropper/src/main/java/com/smarttoolfactory/cropper/util/DimensionUtil.kt
@@ -4,24 +4,24 @@ package com.smarttoolfactory.cropper.util
  * [Linear Interpolation](https://en.wikipedia.org/wiki/Linear_interpolation) function that moves
  * amount from it's current position to start and amount
  * @param start of interval
- * @param end of interval
- * @param amount e closed unit interval [0, 1]
+ * @param stop of interval
+ * @param fraction closed unit interval [0, 1]
  */
-internal fun lerp(start: Float, end: Float, amount: Float): Float {
-    return (1 - amount) * start + amount * end
+fun lerp(start: Float, stop: Float, fraction: Float): Float {
+    return (1 - fraction) * start + fraction * stop
 }
 
 /**
  * Scale x1 from start1..end1 range to start2..end2 range
 
  */
-internal fun scale(start1: Float, end1: Float, pos: Float, start2: Float, end2: Float) =
+fun scale(start1: Float, end1: Float, pos: Float, start2: Float, end2: Float) =
     lerp(start2, end2, calculateFraction(start1, end1, pos))
 
 /**
  * Scale x.start, x.endInclusive from a1..b1 range to a2..b2 range
  */
-internal fun scale(
+fun scale(
     start1: Float,
     end1: Float,
     range: ClosedFloatingPointRange<Float>,
