fd2a2bd8748c4854cc2b0ed529e96920cbc70dc1
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-09-03 16:01:03 +0300

    add Modifier param to ShapeSelection

--- cropper/src/main/java/com/smarttoolfactory/cropper/widget/ShapeSelection.kt
+++ cropper/src/main/java/com/smarttoolfactory/cropper/widget/ShapeSelection.kt
@@ -1,15 +1,14 @@
 package com.smarttoolfactory.cropper.widget
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawOutline
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -20,37 +19,102 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.smarttoolfactory.cropper.model.ShapeModel
 
+
 @Composable
-fun ShapeSelection(isSelected: Boolean, shapeModel: ShapeModel) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(4.dp)) {
-        val density = LocalDensity.current
-        val layoutDirection = LocalLayoutDirection.current
-        val color = if (isSelected) Color.Cyan else Color.LightGray
-        Box(modifier = Modifier
-            .border(3.dp, Color.Red)
-            .size(70.dp)
+fun ShapeSelection(
+    modifier: Modifier = Modifier,
+    color: Color,
+    shapeModel: ShapeModel
+) {
+    Box(
+        modifier = modifier
+            .shadow(1.dp, RoundedCornerShape(8.dp))
+            .background(Color.White)
             .padding(4.dp)
-            .drawWithContent {
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            val density = LocalDensity.current
+            val layoutDirection = LocalLayoutDirection.current
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .aspectRatio(1f)
+                .drawWithContent {
 
-                val outline = shapeModel.shape.createOutline(
-                    size = size,
-                    layoutDirection = layoutDirection,
-                    density = density
-                )
+                    val outline = shapeModel.shape.createOutline(
+                        size = size,
+                        layoutDirection = layoutDirection,
+                        density = density
+                    )
 
-                val width = size.width
-                val height = size.height
-                val outlineWidth = outline.bounds.width
-                val outlineHeight = outline.bounds.height
+                    val width = size.width
+                    val height = size.height
+                    val outlineWidth = outline.bounds.width
+                    val outlineHeight = outline.bounds.height
 
-                translate(
-                    left = (width - outlineWidth) / 2,
-                    top = (height - outlineHeight) / 2
-                ) {
-                    drawOutline(outline = outline, color = color, style = Stroke(3.dp.toPx()))
+                    translate(
+                        left = (width - outlineWidth) / 2,
+                        top = (height - outlineHeight) / 2
+                    ) {
+                        drawOutline(
+                            outline = outline,
+                            color = color,
+                            style = Stroke(3.dp.toPx())
+                        )
+                    }
                 }
-            }
-        )
-        Text(text = shapeModel.title, color = color, fontSize = 14.sp)
+            )
+            Text(text = shapeModel.title, color = color, fontSize = 14.sp)
+        }
+    }
+}
+
+@Composable
+fun ShapeSelection(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean,
+    shapeModel: ShapeModel
+) {
+    Box(
+        modifier = modifier
+            .shadow(1.dp, RoundedCornerShape(8.dp))
+            .background(Color.White)
+            .padding(4.dp)
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            val density = LocalDensity.current
+            val layoutDirection = LocalLayoutDirection.current
+            val color = if (isSelected) Color.Cyan else Color.LightGray
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .aspectRatio(1f)
+                .drawWithContent {
+
+                    val outline = shapeModel.shape.createOutline(
+                        size = size,
+                        layoutDirection = layoutDirection,
+                        density = density
+                    )
+
+                    val width = size.width
+                    val height = size.height
+                    val outlineWidth = outline.bounds.width
+                    val outlineHeight = outline.bounds.height
+
+                    translate(
+                        left = (width - outlineWidth) / 2,
+                        top = (height - outlineHeight) / 2
+                    ) {
+                        drawOutline(
+                            outline = outline,
+                            color = color,
+                            style = Stroke(3.dp.toPx())
+                        )
+                    }
+                }
+            )
+            Text(text = shapeModel.title, color = color, fontSize = 14.sp)
+        }
     }
 }
