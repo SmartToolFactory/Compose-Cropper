f8582217cb844f6b03bfc26b7715117e8c0c478d
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-09-12 15:22:47 +0300

    fix AspectRatios
    add values for aspect ratio in float

--- cropper/src/main/java/com/smarttoolfactory/cropper/model/AspectRatios.kt
+++ cropper/src/main/java/com/smarttoolfactory/cropper/model/AspectRatios.kt
@@ -6,12 +6,12 @@ import com.smarttoolfactory.cropper.util.createRectShape
  * Aspect ratio list with pre-defined aspect ratios
  */
 val aspectRatios = listOf(
-    AspectRatioModel("9:16", createRectShape(9 / 16f)),
-    AspectRatioModel("2:3", createRectShape(2 / 3f)),
-    AspectRatioModel("1:1", createRectShape(1 / 1f)),
-    AspectRatioModel("16:9", createRectShape(16 / 9f)),
-    AspectRatioModel("1.91:1", createRectShape(1.91f / 1f)),
-    AspectRatioModel("3:2", createRectShape(3 / 2f)),
-    AspectRatioModel("3:4", createRectShape(3 / 4f)),
-    AspectRatioModel("3:5", createRectShape(3 / 5f)),
+    AspectRatioModel("9:16", createRectShape(9 / 16f), AspectRatio(9 / 16f)),
+    AspectRatioModel("2:3", createRectShape(2 / 3f), AspectRatio(2 / 3f)),
+    AspectRatioModel("1:1", createRectShape(1 / 1f), AspectRatio(1 / 1f)),
+    AspectRatioModel("16:9", createRectShape(16 / 9f), AspectRatio(16 / 9f)),
+    AspectRatioModel("1.91:1", createRectShape(1.91f / 1f), AspectRatio(1.91f / 1f)),
+    AspectRatioModel("3:2", createRectShape(3 / 2f), AspectRatio(3 / 2f)),
+    AspectRatioModel("3:4", createRectShape(3 / 4f), AspectRatio(3 / 4f)),
+    AspectRatioModel("3:5", createRectShape(3 / 5f), AspectRatio(3 / 5f)),
 )
