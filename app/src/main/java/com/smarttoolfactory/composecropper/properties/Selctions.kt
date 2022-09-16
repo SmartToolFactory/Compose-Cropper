d77cb08a5e9710eaded95280457c622ad3068496
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-09-16 20:41:31 +0300

    add dialog selection

--- app/src/main/java/com/smarttoolfactory/composecropper/properties/Selctions.kt
+++ app/src/main/java/com/smarttoolfactory/composecropper/properties/Selctions.kt
@@ -3,18 +3,18 @@ package com.smarttoolfactory.composecropper.properties
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.RadioButton
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -90,5 +90,151 @@ internal fun FullRowSwitch(
             checked = state,
             onCheckedChange = null
         )
+    }
+}
+
+@Composable
+internal fun DialogWithMultipleSelection(
+    title: String = "",
+    options: List<String>,
+    value: Int,
+    onDismiss: () -> Unit,
+    onConfirm: (Int) -> Unit
+) {
+
+    val (selectedOption: Int, onOptionSelected: (Int) -> Unit) = remember {
+        mutableStateOf(value)
+    }
+
+    AlertDialog(
+        modifier = Modifier.padding(vertical = 8.dp),
+        onDismissRequest = { onDismiss() },
+        title = {
+            Text(
+                title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+        },
+        text = {
+
+            // Note that Modifier.selectableGroup()
+            // is essential to ensure correct accessibility behavior
+            Column(Modifier.selectableGroup()) {
+                options.forEachIndexed { index, text ->
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected = (index == selectedOption),
+                                onClick = { onOptionSelected(index) },
+                                role = Role.RadioButton
+                            )
+                            .padding(vertical = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (index == selectedOption),
+                            onClick = null
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = text,
+                            fontSize = 18.sp,
+                        )
+                    }
+                }
+            }
+        },
+        confirmButton = {
+            Button(
+                onClick = {
+                    onConfirm(selectedOption)
+                }
+            ) {
+                Text(text = "Confirm")
+            }
+        },
+        dismissButton = {
+            Button(
+                onClick = {
+                    onDismiss()
+                }
+            ) {
+                Text(text = "Dismiss")
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun ExposedSelectionMenu(
+    modifier: Modifier = Modifier,
+    index: Int,
+    title: String? = null,
+    textStyle: TextStyle = TextStyle(
+        fontWeight = FontWeight.W600,
+        fontSize = 14.sp
+    ),
+    colors: TextFieldColors = ExposedDropdownMenuDefaults.textFieldColors(
+        containerColor = Color.Transparent,
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        disabledIndicatorColor = Color.Transparent
+    ),
+    options: List<String>,
+    onSelected: (Int) -> Unit
+) {
+
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[index]) }
+
+    ExposedDropdownMenuBox(
+        modifier = modifier,
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        TextField(
+            modifier = modifier,
+            readOnly = true,
+            value = selectedOptionText,
+            onValueChange = { },
+            label = {
+                title?.let {
+                    Text(it)
+                }
+            },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = colors,
+            textStyle = textStyle
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+
+            }
+        ) {
+            options.forEachIndexed { index: Int, selectionOption: String ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectedOptionText = selectionOption
+                        expanded = false
+                        onSelected(index)
+                    },
+                    text = {
+                        Text(text = selectionOption)
+                    }
+                )
+            }
+        }
     }
 }
