6a0ad23c6f2898c626186284685f8146f40c5396
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-09-17 14:38:09 +0300

    update demo

--- app/src/main/java/com/smarttoolfactory/composecropper/demo/ImageCropDemo.kt
+++ app/src/main/java/com/smarttoolfactory/composecropper/demo/ImageCropDemo.kt
@@ -10,13 +10,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Brush
 import androidx.compose.material.icons.filled.Crop
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -26,14 +26,20 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.smarttoolfactory.composecropper.ContentScaleSelectionMenu
 import com.smarttoolfactory.composecropper.ImageSelectionButton
 import com.smarttoolfactory.composecropper.R
+import com.smarttoolfactory.composecropper.preferences.CropStyleSelectionMenu
+import com.smarttoolfactory.composecropper.preferences.PropertySelectionSheet
+import com.smarttoolfactory.cropper.CropDefaults
+import com.smarttoolfactory.cropper.CropProperties
+import com.smarttoolfactory.cropper.CropStyle
 import com.smarttoolfactory.cropper.ImageCropper
 import kotlinx.coroutines.launch
+
+internal enum class SelectionPage {
+    Properties, Style
+}
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -43,6 +49,12 @@ fun ImageCropDemo() {
     val bottomSheetScaffoldState: BottomSheetScaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
     )
+
+    var cropProperties by remember { mutableStateOf(CropDefaults.properties()) }
+    var cropStyle by remember { mutableStateOf(CropDefaults.style()) }
+    val coroutineScope = rememberCoroutineScope()
+
+    var selectionPage by remember { mutableStateOf(SelectionPage.Properties) }
 
     BottomSheetScaffold(
         scaffoldState = bottomSheetScaffoldState,
@@ -56,104 +68,50 @@ fun ImageCropDemo() {
 
         sheetGesturesEnabled = true,
         sheetContent = {
-            SheetContent()
+
+            if (selectionPage == SelectionPage.Properties) {
+                PropertySelectionSheet(
+                    cropProperties = cropProperties,
+                    onCropPropertiesChange = {
+                        cropProperties = it
+                    }
+                )
+            } else {
+                CropStyleSelectionMenu(
+                    cropStyle = cropStyle,
+                    onCropStyleChange = {
+                        cropStyle = it
+                    }
+                )
+            }
         },
 
         // This is the height in collapsed state
         sheetPeekHeight = 0.dp
     ) {
-        MainContent(bottomSheetScaffoldState)
-    }
-
-}
-
-@Composable
-private fun SheetContent() {
-
-
-    Column {
-        Box(
-            modifier = Modifier
-                .padding(vertical = 16.dp)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
+        MainContent(
+            cropProperties,
+            cropStyle,
         ) {
-            Box(
-                modifier = Modifier
-                    .width(30.dp)
-                    .height(5.dp)
-                    .background(Color.LightGray, RoundedCornerShape(50))
-            )
-        }
+            selectionPage = it
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(400.dp)
-                .padding(horizontal = 16.dp, vertical = 8.dp)
-                .verticalScroll(rememberScrollState())
-        ) {
-
-
-            Title("Crop Type")
-            Text("Under Development")
-
-            Title("Aspect Ratio")
-            Text("Under Development")
-
-            // Handle size and overlay size applies only to Dynamic crop
-            Title("Handle Size")
-            Text("Under Development")
-
-            Title("Minimum Overlay Size")
-            Text("Under Development")
-
-            Title("Draw Overlay")
-            Text("Under Development")
-
-            Title("Overlay StrokeWidth")
-            Text("Under Development")
-
-            Title("Overlay Color")
-            Text("Under Development")
-
-            Title("Draw Grid")
-            Text("Under Development")
-
-            Title("Content Scale")
-            Text("Under Development")
-
-            Title("Min Zoom")
-            Text("Under Development")
-
-            Title("Max Zoom")
-            Text("Under Development")
-
-            Title("Fling")
-            Text("Under Development")
-
-            Title("Pan Enabled")
-            Text("Under Development")
-
-            Title("Zoom Enabled")
-            Text("Under Development")
+            coroutineScope.launch {
+                if (bottomSheetScaffoldState.bottomSheetState.isExpanded) {
+                    bottomSheetScaffoldState.bottomSheetState.collapse()
+                } else {
+                    bottomSheetScaffoldState.bottomSheetState.expand()
+                }
+            }
         }
     }
 }
 
 @Composable
-private fun Title(text: String) {
-    Text(
-        modifier = Modifier.padding(vertical = 1.dp),
-        text = text,
-        color = MaterialTheme.colorScheme.primary,
-        fontSize = 18.sp,
-        fontWeight = FontWeight.Bold
-    )
-}
-
-@Composable
-private fun MainContent(bottomSheetScaffoldState: BottomSheetScaffoldState) {
+private fun MainContent(
+    cropProperties: CropProperties,
+    cropStyle: CropStyle,
+    onSelectionPageMenuClicked: (SelectionPage) -> Unit
+) {
 
     val imageBitmapLarge = ImageBitmap.imageResource(
         LocalContext.current.resources,
@@ -168,12 +126,7 @@ private fun MainContent(bottomSheetScaffoldState: BottomSheetScaffoldState) {
         .fillMaxWidth()
         .aspectRatio(3 / 4f)
 
-    var contentScale by remember { mutableStateOf(ContentScale.Fit) }
-
-    val coroutineScope = rememberCoroutineScope()
-
     var crop by remember { mutableStateOf(false) }
-
     var showDialog by remember { mutableStateOf(false) }
 
 
@@ -184,15 +137,12 @@ private fun MainContent(bottomSheetScaffoldState: BottomSheetScaffoldState) {
                 .verticalScroll(rememberScrollState())
         ) {
 
-            ContentScaleSelectionMenu(contentScale) {
-                contentScale = it
-            }
-
             ImageCropper(
                 modifier = modifier,
                 imageBitmap = imageBitmap,
-                contentScale = contentScale,
                 contentDescription = "Image Cropper",
+                cropStyle = cropStyle,
+                cropProperties = cropProperties,
                 crop = crop,
             ) {
                 croppedImage = it
@@ -205,26 +155,29 @@ private fun MainContent(bottomSheetScaffoldState: BottomSheetScaffoldState) {
             modifier = Modifier.align(Alignment.BottomStart),
             actions = {
 
+
                 IconButton(
                     onClick = {
-                        coroutineScope.launch {
-                            if (bottomSheetScaffoldState.bottomSheetState.isExpanded) {
-                                bottomSheetScaffoldState.bottomSheetState.collapse()
-                            } else {
-                                bottomSheetScaffoldState.bottomSheetState.expand()
-                            }
-                        }
+                        onSelectionPageMenuClicked(SelectionPage.Properties)
                     }
                 ) {
                     Icon(
-                        Icons.Filled.Edit,
-                        contentDescription = "Localized description",
+                        Icons.Filled.Settings,
+                        contentDescription = "Settings",
                     )
+
+                }
+                IconButton(
+                    onClick = {
+                        onSelectionPageMenuClicked(SelectionPage.Style)
+                    }
+                ) {
+                    Icon(Icons.Filled.Brush, contentDescription = "Style")
                 }
 
                 IconButton(
                     onClick = { crop = true }) {
-                    Icon(Icons.Filled.Crop, contentDescription = "Localized description")
+                    Icon(Icons.Filled.Crop, contentDescription = "Crop Image")
                 }
             },
             floatingActionButton = {
--- cropper/src/main/java/com/smarttoolfactory/cropper/model/AspectRatios.kt
+++ cropper/src/main/java/com/smarttoolfactory/cropper/model/AspectRatios.kt
@@ -6,12 +6,13 @@ import com.smarttoolfactory.cropper.util.createRectShape
  * Aspect ratio list with pre-defined aspect ratios
  */
 val aspectRatios = listOf(
-    AspectRatioModel("9:16", createRectShape(9 / 16f), AspectRatio(9 / 16f)),
-    AspectRatioModel("2:3", createRectShape(2 / 3f), AspectRatio(2 / 3f)),
-    AspectRatioModel("1:1", createRectShape(1 / 1f), AspectRatio(1 / 1f)),
-    AspectRatioModel("16:9", createRectShape(16 / 9f), AspectRatio(16 / 9f)),
-    AspectRatioModel("1.91:1", createRectShape(1.91f / 1f), AspectRatio(1.91f / 1f)),
-    AspectRatioModel("3:2", createRectShape(3 / 2f), AspectRatio(3 / 2f)),
-    AspectRatioModel("3:4", createRectShape(3 / 4f), AspectRatio(3 / 4f)),
-    AspectRatioModel("3:5", createRectShape(3 / 5f), AspectRatio(3 / 5f)),
+    AspectRatioModel("9:16", createRectShape(AspectRatio(9 / 16f)), AspectRatio(9 / 16f)),
+    AspectRatioModel("2:3", createRectShape(AspectRatio(2 / 3f)), AspectRatio(2 / 3f)),
+    AspectRatioModel("Full", createRectShape(AspectRatio.Unspecified), AspectRatio.Unspecified),
+    AspectRatioModel("1:1", createRectShape(AspectRatio(1 / 1f)), AspectRatio(1 / 1f)),
+    AspectRatioModel("16:9", createRectShape(AspectRatio(16 / 9f)), AspectRatio(16 / 9f)),
+    AspectRatioModel("1.91:1", createRectShape(AspectRatio(1.91f / 1f)), AspectRatio(1.91f / 1f)),
+    AspectRatioModel("3:2", createRectShape(AspectRatio(3 / 2f)), AspectRatio(3 / 2f)),
+    AspectRatioModel("3:4", createRectShape(AspectRatio(3 / 4f)), AspectRatio(3 / 4f)),
+    AspectRatioModel("3:5", createRectShape(AspectRatio(3 / 5f)), AspectRatio(3 / 5f))
 )
