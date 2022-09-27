2929c8b8d5979f04d66f28da1ff0ff766ab61e48
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-09-27 13:52:53 +0300

    add function to draw checker background

--- cropper/src/main/java/com/smarttoolfactory/cropper/util/DrawScopeUtils.kt
+++ cropper/src/main/java/com/smarttoolfactory/cropper/util/DrawScopeUtils.kt
@@ -2,8 +2,10 @@ package com.smarttoolfactory.cropper.util
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.unit.dp
 
 
 fun DrawScope.drawGrid(rect: Rect, strokeWidth: Float, color: Color) {
@@ -34,3 +36,26 @@ fun DrawScope.drawGrid(rect: Rect, strokeWidth: Float, color: Color) {
     }
 }
 
+
+fun DrawScope.drawChecker() {
+    val width = this.size.width
+    val height = this.size.height
+
+    val checkerWidth = 10.dp.toPx()
+
+    val checkerHeight = 10.dp.toPx()
+
+    val horizontalSteps = (width / checkerWidth).toInt()
+    val verticalSteps = (height / checkerHeight).toInt()
+
+    for (y in 0..verticalSteps) {
+        for (x in 0..horizontalSteps) {
+            val isGrayTile = ((x + y) % 2 == 1)
+            drawRect(
+                color = if (isGrayTile) Color.LightGray else Color.White,
+                topLeft = Offset(x * checkerWidth, y * checkerHeight),
+                size = Size(checkerWidth, checkerHeight)
+            )
+        }
+    }
+}
