45a491838af2dc4a287a38943a637e51a96bd315
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-10-01 18:16:10 +0300

    update text position in ImageCropper for debugging

--- cropper/src/main/java/com/smarttoolfactory/cropper/ImageCropper.kt
+++ cropper/src/main/java/com/smarttoolfactory/cropper/ImageCropper.kt
@@ -2,7 +2,7 @@ package com.smarttoolfactory.cropper
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -192,7 +192,7 @@ fun ImageCropper(
 
             // TODO Remove this text when cropper is complete. This is for debugging
             Text(
-                modifier = Modifier.align(Alignment.BottomStart),
+                modifier = Modifier.align(Alignment.TopStart),
                 color = Color.White,
                 fontSize = 10.sp,
                 text = "imageWidthInPx: $imageWidthPx, imageHeightInPx: $imageHeightPx\n" +
